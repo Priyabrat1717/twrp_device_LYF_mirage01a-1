29ce620f77973e6296932c1f711327e85238917d
PS1717 <38763217+Priyabrat1717@users.noreply.github.com>
2019-09-17 20:55:45 +0530

    Rename batik to omni

--- omni_mirage01a.mk
+++ omni_mirage01a.mk
@@ -20,7 +20,7 @@
 $(call inherit-product, device/LYF/mirage01a/full_mirage01a.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/batik/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 PRODUCT_NAME := omni_mirage01a
 BOARD_VENDOR := LYF
